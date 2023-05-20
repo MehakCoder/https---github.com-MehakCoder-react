99c95bb12dbf5814163d15da433c9d5a2c980065
MehakCoder <cmehak26@gmail.com>
2023-05-20 22:36:51 +0530

    add new folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
+     
      <Header></Header>
+
+
     </div>
   );
 }
---
+++ src/components/From.js
@@ -0,0 +1,11 @@
+const Form =()=>{
+    return(
+        <div>
+            <Form>
+                <input placeholder="Add Todo Item"></input>
+                <button>Add to project</button>
+            </Form>
+        </div>
+    )
+}
+export default Form;
